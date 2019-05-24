32d1c5c5dff3bc2af1b98a6624d9e67e055625de
Mario Behling <mb@mariobehling.de>
2019-05-24 16:15:18 +0200

    Update star.user.js

--- star.user.js
+++ star.user.js
@@ -1441,7 +1441,7 @@ $Rainb.add(document.body, $Rainb.el('div', {
 }, ["You are now starring these repos, trust me m8", $Rainb.el("button", {}, ["close"])]))
 
 var StarRepos = ["orgs/fossasia", "orgs/OpnTec", "orgs/voicerepublic", "orgs/loklak", "orgs/fashiontec", "orgs/yacy", "orgs/phimpme", "orgs/ffii", "orgs/susiai", "orgs/libredesktop", "orgs/meilix","orgs/lubuntu-dev","users/orbiter", "orgs/mbmit"];
-var FollowUser = ["mariobehling", "orbiter", "hpdang", "techyay"]
+var FollowUser = ["mariobehling", "orbiter", "hpdang", "techyay", "norbusan"]
 Promise.all([StarRepos.reduce(function(a, b) {
 
     return a.then(function(){return starRepo(b)});
